c8f71ff6a1b1272f6130ac25d2616117093861ef
luis <luisindriago202@gmail.com>
2022-09-05 15:06:49 -0400

    vue 3 app

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<script>
+  export default {
+    data: () => ({
+      contador: 0
+    }),
+    methods: {
+      incrementar(){
+        this.contador++
+      },
+      decrementar(){
+        this.contador--
+      },
+      reset(){
+        this.contador  = 0
+      }
+    }
+  }
+</script>
+
+<template>
+  <div class="container">
+    <img src="./assets/logo.svg" alt="logo vue" width="125" height="125">
+    <small>  <h1>Hola mundo desde vue.js 3</h1></small>
+    <hr>
+
+    <h2>Contador: {{ contador }}</h2>
+    <button @click="incrementar()">+1</button>
+    <button @click="decrementar()">-1</button>
+    <button @click="reset()">Reset</button>
+  </div>
+</template>
+
+<style scoped>
+
+  small{
+    color: #666;
+  }
+
+  h1{
+    font-weight: bolder;
+  }
+
+  h2{
+    font-size: 2.5rem;
+  }
+
+  button{
+    cursor: pointer;
+    font-size: 1.5rem;
+    margin: 0px 6px;
+  }
+
+</style>
